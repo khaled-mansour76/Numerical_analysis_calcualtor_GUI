472301be7634bc2d5cc38ace2d2afa55f1b90878
khaled mansour <hsohcl71@gmail.com>
2026-03-03 14:02:41 +0200

    I finshied Type OF error and Condition Number

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fractions import Fraction
-
+import sympy as sp
 def TypeOferrors( XE ,  XA):
     try:
         
@@ -38,11 +38,30 @@ def TypeOferrors( XE ,  XA):
     except ValueError():
         print("Pleas Ente Float Number Correct  \n")
 
+def calculate_condition_number(equation, A):
+    x = sp.symbols('x')  #define X
+    
+    try:
+        f = sp.sympify(equation)  #transform the string to equation
+    except:
+        return ("EX. sin x -> sin(x)")
+    
+    d = sp.diff(f, x)
+    
+    try:
+        Ae = float(sp.sympify(A, locals={'pi': sp.pi}).evalf())
+    except:
+        return ("Enter A correct Ex. 𝜋 -> pi")
 
+    r1 = f.subs(x, Ae).evalf()  #calculate f(a)
+    r2 = d.subs(x, Ae).evalf()  #calclate f'(a)
+    
+    if r1 == 0:
+        return("f(a) = 0")
+    
 
+    print(f"The Original  Function -> {f} \n ") ; print(f"The Derintive Function -> {d} \n") ; print(f"a Value {Ae} Original a {A}")
+    
 
-
-x = Fraction(input("enter XE \t"))
-y = Fraction(input("enter XA \t"))
-
-TypeOferrors(x,y)
+    result = abs((Ae * r2) / r1)
+    return float(result)
