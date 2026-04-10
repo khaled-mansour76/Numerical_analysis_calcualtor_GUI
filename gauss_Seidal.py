00e80e7e444f30b2d94228c3673256384c9b80d6
Khaled Mansour <khaledmansour0018@gmail.com>
2026-04-10 10:19:52 +0200

    Refactor Gauss-Seidel method for dynamic input and change the name
    Refactor Gauss-Seidel method to handle dynamic dimensions and return iteration history as a DataFrame.

--- gauss_Seidal.py
+++ gauss_Seidal.py
@@ -1,58 +1,52 @@
 import numpy as np
 from itertools import permutations
+import pandas as pd
 
 def order(A, b):
     A = np.array(A, dtype=float)
     b = np.array(b, dtype=float)
     n = len(b)
-    
     for p in permutations(range(n)):
         New_order_of_A = A[list(p)]
         New_order_of_b = b[list(p)]
-        
         Bool = True
         for i in range(n):
             diag = abs(New_order_of_A[i, i])
             Sum = sum(abs(New_order_of_A[i, j]) for j in range(n) if i != j)
-            
             if diag < Sum:
                 Bool = False
                 break
-        
         if Bool:
             return New_order_of_A, New_order_of_b
-            
     return A, b
 
 def gauss_seidel(A, B):
     A, B = order(A, B)
-    
-    x, y, z = 0.0, 0.0, 0.0
+    n = len(B)
+    x = np.zeros(n)
     iteration = 0 
     max_iter = 100
+    history = []
 
     while iteration < max_iter:
         iteration += 1
+        old_x = x.copy()
+
+        for i in range(n):
+            sum_val = 0
+            for j in range(n):
+                if i != j:
+                    sum_val += A[i, j] * x[j]
+            x[i] = (B[i] - sum_val) / A[i, i]
         
-        old_x, old_y, old_z = x, y, z
+        history.append(list(x))
 
-        x = (B[0] - (A[0,1] * y) - (A[0,2] * z)) / A[0,0]
-        
-        y = (B[1] - (A[1,0] * x) - (A[1,2] * z)) / A[1,1]
-        
-        z = (B[2] - (A[2,0] * x) - (A[2,1] * y)) / A[2,2]
+        if np.allclose(x, old_x, atol=1e-6):
+            break
 
-        if np.isclose(x, old_x, atol=1e-6) and \
-           np.isclose(y, old_y, atol=1e-6) and \
-           np.isclose(z, old_z, atol=1e-6):
-            
-            return x, y, z, iteration
+    column_names = [f"X{i+1}" for i in range(n)]
+    df = pd.DataFrame(history, columns=column_names)
+    df.index.name = "Iteration"
+    df.index += 1
 
-    return x, y, z, iteration 
-
-x, y, z, i = gauss_seidel([[1,-8,3], [8,2,-2],[2,1,9]], [-4,8,12])
-print(f"--- Jacobi Result ---")
-print(f"x = {x}")
-print(f"y = {y}")
-print(f"z = {z}")
-print(f"iterations = {i}")
+    return x, iteration, df
